3961495c36fc5368e2e80e93b7be0aa8aca54807
Perishte <javascript_9595@bk.ru>
2023-11-24 07:03:07 +0500

    added function for remove posts through state

--- src/App.js
+++ src/App.js
@@ -7,80 +7,83 @@ import label from './components/info-list/style.module.css';
 import input from './components/info-list/style.module.css';
 import btn from './components/info-list/style.module.css';
 import wrap2 from './components/info-list/style.module.css';
+import listItem from './components/info-list/style.module.css';
+import deleteBtn from './components/info-list/style.module.css';
+import mainTitle from './components/info-list/style.module.css';
 
 function App() {
-  const DATA = [
-    {
-      id: 1,
-      title: 'lorem ipsum dolore 1',
-      body: 'lorem lorem lorem lorem'
-    },
-    {
-      id: 2,
-      title: 'lorem ipsum dolore 2',
-      body: 'lorem lorem lorem lorem'
-    },
-    {
-      id: 3,
-      title: 'lorem ipsum dolore 3',
-      body: 'lorem lorem lorem lorem'
-    },
-    {
-      id: 4,
-      title: 'lorem ipsum dolore 4',
-      body: 'lorem lorem lorem lorem'
-    },
-  ]
+  // Array from objects
+  const DATA = [{
+    id: 1, title: 'lorem ipsum dolore 1', body: 'lorem lorem lorem lorem'
+  }, {
+    id: 2, title: 'lorem ipsum dolore 2', body: 'lorem lorem lorem lorem'
+  }, {
+    id: 3, title: 'lorem ipsum dolore 3', body: 'lorem lorem lorem lorem'
+  }, {
+    id: 4, title: 'lorem ipsum dolore 4', body: 'lorem lorem lorem lorem'
+  },]
+  // The end of Array from objects
+  //All states
   const [data, setData] = useState(DATA);
   const [newTitle, setNewTitle] = useState('');
+  //The end of all states
+  //Handle functions for events
   function handleTitleChange(e) {
-    setNewTitle(e.target.value);;
+    setNewTitle(e.target.value);
   }
+
   function handleAddPost() {
     const newData = {
-      id: data.length + 1,
-      title: newTitle,
+      id: Math.round(Math.random(5) * 20), title: newTitle,
     }
     setData((data) => [...data, newData]);
     setNewTitle('');
+    console.log(data);
   }
 
-  return (
-    <div className={styles["container"]}>
-      <div className={wrap2['wrap2']}>
-        {/*{DATA.map((item) => {*/}
-        {/*  return (*/}
-        {/*    <InfoList*/}
-        {/*      key={item.id}*/}
-        {/*      title={item.title}*/}
-        {/*      body={item.body}*/}
-        {/*    />*/}
-        {/*  )*/}
-        {/*})}*/}
-          <div className={form['form']}>
-            <label className={label['label']}>
-              Enter New Post:
-              <input value={newTitle}
-                     type="text"
-                     placeholder="Enter new post..."
-                     className={input['input']}
-                     onChange={handleTitleChange}
-              />
-            </label>
-            <button className={btn['btn']} onClick={handleAddPost}>Add</button>
-          </div>
-        <ul className={styled['list']}>
-          {data.map(item => {
-            return(
-              <li key={item.id}>
-                <h3>{item.title}</h3>
-              </li>
-            )
-          })}
-        </ul>
+  //End of Handle functions for events
+  //Render Info
+  return (<div className={styles["container"]}>
+    <div className={wrap2['wrap2']}>
+      {/*{DATA.map((item) => {*/}
+      {/*  return (*/}
+      {/*    <InfoList*/}
+      {/*      key={item.id}*/}
+      {/*      title={item.title}*/}
+      {/*      body={item.body}*/}
+      {/*    />*/}
+      {/*  )*/}
+      {/*})}*/}
+      <div className={form['form']}>
+        <label className={label['label']}>
+          Enter New Post:
+          <input value={newTitle}
+                 type="text"
+                 placeholder="Enter new post..."
+                 className={input['input']}
+                 onChange={handleTitleChange}
+          />
+        </label>
+        <button className={btn['btn']} onClick={handleAddPost}>Add</button>
       </div>
+      <ul className={styled['list']}>
+        {data.map(item => {
+          return (<li key={item.id} className={listItem['listItem']}>
+            <h3 className={mainTitle['mainTitle']}>{item.title}</h3>
+            <button onClick={() => {
+              setData(data.filter(post => post.id !== item.id))
+            }} className={deleteBtn['deleteBtn']}>Delete
+            </button>
+          </li>)
+        })}
+      </ul>
     </div>
-  );
+  </div>);
 }
 
+//End of Render Info
+
 export default App;
+
+
+
--- src/components/info-list/index.jsx
+++ src/components/info-list/index.jsx
@@ -2,14 +2,15 @@ import React from 'react';
 import styles from './style.module.css';
 
 const InfoList = (props) => {
-  return(
+  //get values from parent component through props
+  return (
     <div className={styles['wrap']}>
       <p>{props.key}</p>
       <p>{props.title}</p>
       <p>{props.body}</p>
-
-
     </div>
   )
+  //The end of get values from parent component through props
 }
 export default InfoList;
+
