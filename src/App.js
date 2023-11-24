3e9e98a14bc8d2db391bed4ebf0bc3cf9ea4224c
Perishte <javascript_9595@bk.ru>
2023-11-24 06:17:56 +0500

    added new functional adding new post into array through state

--- src/App.js
+++ src/App.js
@@ -1,46 +1,84 @@
-import './App.css';
+import {useState} from 'react';
 import InfoList from "./components/info-list";
-
-const DATA = [
-  {
-    id: 1,
-    title: 'lorem ipsum dolore 1',
-    body: 'lorem lorem lorem lorem'
-  },
-  {
-    id: 2,
-    title: 'lorem ipsum dolore 2',
-    body: 'lorem lorem lorem lorem'
-  },
-  {
-    id: 3,
-    title: 'lorem ipsum dolore 3',
-    body: 'lorem lorem lorem lorem'
-  },
-  {
-    id: 4,
-    title: 'lorem ipsum dolore 4',
-    body: 'lorem lorem lorem lorem'
-  },
-  {
-    id: 5,
-    title: 'lorem ipsum dolore 5',
-    body: 'lorem lorem lorem lorem'
-  },
-];
+import styles from './components/info-list/style.module.css';
+import styled from './components/info-list/style.module.css';
+import form from './components/info-list/style.module.css';
+import label from './components/info-list/style.module.css';
+import input from './components/info-list/style.module.css';
+import btn from './components/info-list/style.module.css';
+import wrap2 from './components/info-list/style.module.css';
 
 function App() {
+  const DATA = [
+    {
+      id: 1,
+      title: 'lorem ipsum dolore 1',
+      body: 'lorem lorem lorem lorem'
+    },
+    {
+      id: 2,
+      title: 'lorem ipsum dolore 2',
+      body: 'lorem lorem lorem lorem'
+    },
+    {
+      id: 3,
+      title: 'lorem ipsum dolore 3',
+      body: 'lorem lorem lorem lorem'
+    },
+    {
+      id: 4,
+      title: 'lorem ipsum dolore 4',
+      body: 'lorem lorem lorem lorem'
+    },
+  ]
+  const [data, setData] = useState(DATA);
+  const [newTitle, setNewTitle] = useState('');
+  function handleTitleChange(e) {
+    setNewTitle(e.target.value);;
+  }
+  function handleAddPost() {
+    const newData = {
+      id: data.length + 1,
+      title: newTitle,
+    }
+    setData((data) => [...data, newData]);
+    setNewTitle('');
+  }
+
   return (
-    <div className='wrap2'>
-      {DATA.map((item) => {
-        return (
-          <InfoList
-            key={item.id}
-            title={item.title}
-            body={item.body}
-          />
-        )
-      })}
+    <div className={styles["container"]}>
+      <div className={wrap2['wrap2']}>
+        {/*{DATA.map((item) => {*/}
+        {/*  return (*/}
+        {/*    <InfoList*/}
+        {/*      key={item.id}*/}
+        {/*      title={item.title}*/}
+        {/*      body={item.body}*/}
+        {/*    />*/}
+        {/*  )*/}
+        {/*})}*/}
+          <div className={form['form']}>
+            <label className={label['label']}>
+              Enter New Post:
+              <input value={newTitle}
+                     type="text"
+                     placeholder="Enter new post..."
+                     className={input['input']}
+                     onChange={handleTitleChange}
+              />
+            </label>
+            <button className={btn['btn']} onClick={handleAddPost}>Add</button>
+          </div>
+        <ul className={styled['list']}>
+          {data.map(item => {
+            return(
+              <li key={item.id}>
+                <h3>{item.title}</h3>
+              </li>
+            )
+          })}
+        </ul>
+      </div>
     </div>
   );
 }
